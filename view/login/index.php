b46242ec19dc9211a82691b99e99016c025d5428
remysiri <remysiri@student.arteveldehs.be>
2019-06-04 12:14:39 +0200

    login page styling

--- view/login/index.php
+++ view/login/index.php
@@ -1,17 +1,18 @@
-<form action="index.php?page=login" method="POST">
-<input type="hidden" name="action" value="login"/> 
-    <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" />
-        <span class="username_error"></span>
-    </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <span class="password_error"></span>
-    </div>
-    <div class="form-group">
-        <button type="submit">Login</button>
-    </div>
-    <p>No account yet?<a href="index.php?page=register">Sign Up</a></p>
-</form>
+<div class="auth__container">
+    <form action="index.php?page=login" method="POST">
+    <input type="hidden" name="action" value="login"/> 
+        <div class="form-group">
+            <input type="text" name="username" id="username" placeholder="Username"/>
+            <span class="username_error"></span>
+        </div>
+        <div class="form-group">
+            <input type="password" name="password" id="password" placeholder="Password"/>
+            <span class="password_error"></span>
+        </div>
+        <div class="form-group">
+            <button class="btn auth-btn" type="submit">Login</button>
+        </div>
+    </form>
+</div>
+
+<p class="login__signup">No account yet? <a class="red-bold" href="index.php?page=register">Sign Up</a></p>
